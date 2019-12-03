79be32bf1d296bf7ce21d393dbef7126333b00ae
kms70847 <kevin.m.smiley@gmail.com>
2019-12-02 22:37:34 -0500

    06.2

--- 06/main.py
+++ 06/main.py
@@ -34,7 +34,7 @@ for i in range(left, right+1):
             field[i,j] = claimants[0]
             counts[claimants[0]] += 1
 
-#coords that own a tile on the edge have infinite area (I think)
+#coords that own a tile on the edge have infinite area (probably)
 edges = []
 for i in range(left, right+1):
     for j in [top, bottom]:
@@ -47,3 +47,27 @@ for c in edges:
         counts[field[c]] = float("inf")
 
 print(max(v for v in counts.values() if v != float("inf")))
+
+
+
+#part 2
+field = {}
+for i in range(left, right+1):
+    for j in range(top, bottom+1):
+        field[i,j] = sum(manhattan(coord, (i,j)) for coord in coords)
+
+#calculate valid cells within the minimal bounding box of the coordinates
+total = sum(1 for v in field.values() if v < 10000)
+
+#walk along the edge of the bounding box. If a border cell is valid, determine how many more valid cells lie in that row or column.
+for p in edges:
+    if field[p] < 10000:
+        total += (10000 - field[i,top]) // len(coords)
+
+#regions diagonal to the bounding box can be calculated just from the corner value.
+for p in [(left, top), (right, top), (right, bottom), (left, bottom)]:
+    if field[p] < 10000:
+        n = (10000 - field[i,top]) // len(coords)
+        total += (n**2 + n)//2
+    
+print(total)
